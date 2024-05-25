65ef351bc790bb605d3453dc80117e8ada066fad
Doaa Abdelazez <abdelazezdoaa30@gmail.com>
2024-05-25 16:43:25 +0300

    "Refactored SearchScreen.dart: added ImagePicker import, rearranged widget structure, and modified UI elements"

--- lib/Features/home/presentation/Screens/search_Screen.dart
+++ lib/Features/home/presentation/Screens/search_Screen.dart
@@ -1,8 +1,10 @@
 import 'dart:io';
+
 import 'package:flutter/material.dart';
+
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-
+import 'package:image_picker/image_picker.dart';
 import 'package:our_children/Features/home/presentation/home_cubit/cubit/home_cubit.dart';
 import 'package:our_children/core/functions/navigation.dart';
 import 'package:our_children/core/utils/app_assets.dart';
@@ -36,43 +38,44 @@ class _SearchScreenState extends State<SearchScreen> {
               Padding(
                 padding: const EdgeInsets.only(top: 250),
                 child: SingleChildScrollView(
-                  child: BlocProvider.of<HomeCubit>(context).searchPic == null
-                      ? Column(
-                          children: [
-                            //! search_Text
-                            Padding(
-                              padding: const EdgeInsets.only(top: 10),
-                              child: Text(
-                                AppStrings.search,
-                                style: CustomTextStyle.cairo400style30,
-                              ),
-                            ),
-                            SizedBox(
-                              height: 12.h,
-                            ),
-                            //!ImagePicker
-                            Center(
-                              child: Container(
-                                alignment: Alignment.center,
-                                width: 300.w,
-                                height: 250.h,
-                                decoration: BoxDecoration(
-                                    image: BlocProvider.of<HomeCubit>(context)
-                                                .searchPic ==
-                                            null
-                                        ? null
-                                        : DecorationImage(
-                                            image: FileImage(File(
-                                                BlocProvider.of<HomeCubit>(
-                                                        context)
-                                                    .searchPic!
-                                                    .path))),
-                                    color: AppColors.white,
-                                    borderRadius: BorderRadius.circular(15)),
-                                child: Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: SingleChildScrollView(
-                                    child: Column(
+                  child: Column(
+                    children: [
+                      //! search_Text
+                      Padding(
+                        padding: const EdgeInsets.only(top: 10),
+                        child: Text(
+                          AppStrings.search,
+                          style: CustomTextStyle.cairo400style30,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 12.h,
+                      ),
+                      //!ImagePicker
+                      Center(
+                        child: Container(
+                          alignment: Alignment.center,
+                          width: 300.w,
+                          height: 250.h,
+                          decoration: BoxDecoration(
+                              image: BlocProvider.of<HomeCubit>(context)
+                                          .searchPic ==
+                                      null
+                                  ? null
+                                  : DecorationImage(
+                                      image: FileImage(File(
+                                          BlocProvider.of<HomeCubit>(context)
+                                              .searchPic!
+                                              .path))),
+                              color: AppColors.white,
+                              borderRadius: BorderRadius.circular(15)),
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: SingleChildScrollView(
+                              child: BlocProvider.of<HomeCubit>(context)
+                                          .searchPic ==
+                                      null
+                                  ? Column(
                                       children: [
                                         //!clickhere text
                                         Text(
@@ -92,30 +95,30 @@ class _SearchScreenState extends State<SearchScreen> {
                                           ),
                                         ),
                                       ],
-                                    ),
-                                  ),
-                                ),
-                              ),
+                                    )
+                                  : Container(),
                             ),
-                            SizedBox(
-                              height: 100.h,
-                            ),
-                            //!bttn
-                            CustomButtnHome(
-                              onPressed: () {
-                                // if (BlocProvider.of<HomeCubit>(context)
-                                //     .searchKey
-                                //     .currentState!
-                                //     .validate()) {
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 100.h,
+                      ),
+                      //!bttn
+                      CustomButtnHome(
+                        onPressed: () {
+                          // if (BlocProvider.of<HomeCubit>(context)
+                          //     .searchKey
+                          //     .currentState!
+                          //     .validate()) {
 
-                                // }
-                                customNavigate(context, ('/ResulteScreen'));
-                              },
-                              text: AppStrings.done,
-                            ),
-                          ],
-                        )
-                      : Container(),
+                          // }
+                          customNavigate(context, ('/ResulteScreen'));
+                        },
+                        text: AppStrings.done,
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ],
